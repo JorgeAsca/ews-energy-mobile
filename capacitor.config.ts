13e7f6b121555aeefc00418bbf5fe12153ef1bac
JorgeAsca <jascola171@g.educaand.es>
2026-04-21 10:44:06 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ews-energy-mobile',
+  webDir: 'dist'
+};
+
+export default config;
